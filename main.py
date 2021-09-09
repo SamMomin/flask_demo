929916bc47cd49b6191b5e34fcb4761e0b68a4f0
Sana <momin.sana24@gmail.com>
2021-09-09 15:13:42 +0530

    changed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return "<h1>This is my first flask application</h1>"
+    return "<h1><i>This is my first flask application</i></h1>"
 
 if __name__=="__main__":
     app.run()
